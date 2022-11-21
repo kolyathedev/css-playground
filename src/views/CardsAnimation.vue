1368e3846214493700d03d9de712ffc6bea69938
Niko <63638951+NikoGham@users.noreply.github.com>
2022-11-21 06:56:23 +0000

    cards

---
+++ src/views/CardsAnimation.vue
@@ -0,0 +1,109 @@
+<template>
+	<div class="flex-container">
+		<div style="flex-grow: 1">1 1/3</div>
+		<div style="flex-grow: 1">2 1/3</div>
+		<div style="flex-grow: 1">3 1/3</div>
+	</div>
+	<div class="flex-container">
+		<div style="flex-grow: 1">
+			<div class="card">
+				<img src="../assets/logo.png" alt="logo" />
+				<div class="container">
+					<h4>hey mom</h4>
+					<p>living la vie da loca</p>
+				</div>
+			</div>
+		</div>
+		<div style="flex-grow: 1">
+			<div class="card">
+				<img src="../assets/logo.png" alt="logo" />
+				<div class="container">
+					<h4>hey mom</h4>
+					<p>living la vie da loca</p>
+				</div>
+			</div>
+		</div>
+		<div style="flex-grow: 1">
+			<div class="card">
+				<img src="../assets/logo.png" alt="logo" />
+				<div class="container">
+					<h4>hey mom</h4>
+					<p>living la vie da loca</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-m-4">
+			<div class="card">
+				<img src="../assets/logo.png" alt="logo" />
+				<div class="container">
+					<h4>hey mom</h4>
+					<q>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam nihil
+						non molestias fuga hic quos commodi quod veritatis? Vel velit laudantium
+						perferendis repudiandae quod ducimus aut dolorum fugiat consequatur
+						necessitatibus.
+					</q>
+					<p>living la vie da loca</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-m-4">
+			<div class="card">
+				<img src="../assets/logo.png" alt="logo" />
+				<div class="container">
+					<h4>hey mom</h4>
+					<q>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam nihil
+						non molestias fuga hic quos commodi quod veritatis? Vel velit laudantium
+						perferendis repudiandae quod ducimus aut dolorum fugiat consequatur
+						necessitatibus.
+					</q>
+					<p>living la vie da loca</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-m-4">
+			<div class="card">
+				<img src="../assets/logo.png" alt="logo" />
+				<div class="container">
+					<h4>hey mom</h4>
+					<q>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam nihil
+						non molestias fuga hic quos commodi quod veritatis? Vel velit laudantium
+						perferendis repudiandae quod ducimus aut dolorum fugiat consequatur
+						necessitatibus.
+					</q>
+					<p>living la vie da loca</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script lang="ts"></script>
+<style lang="scss" scoped>
+.card {
+	box-shadow: 0 4px 8px 0 rgba($color: #000000, $alpha: 0.2);
+	transition: 0.5s;
+	background: #fba92c;
+	color: #000000;
+	cursor: pointer;
+	border-radius: 10px;
+	padding: 10px;
+	&:hover {
+		box-shadow: 0 8px 16px 0 rgba($color: #000000, $alpha: 1);
+		background: #ffffff;
+	}
+	display: grid;
+	grid-template-rows: repeat(2, 1fr);
+	img {
+		place-self: center;
+	}
+	.container {
+		text-align: left;
+		padding: 10px;
+	}
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -91,7 +91,7 @@
 		</div>
 		<div class="col-m-1">1</div>
 	</div>
-	<div class="flex-container">
+	<div class="flex-container" title="flex-box">
 		<div style="flex-grow: 1; order: 3">Flex child order 3, flex grow</div>
 		<div style="flex-grow: 1; order: 1; align-self: flex-end">
 			order 1, align self flex end
@@ -103,41 +103,205 @@
 			order 4, align self flex start, flex grow 3
 		</div>
 	</div>
-	<div class="grid-container-block">
-		<div class="flex-item item-1">Block display grid container</div>
+	<div
+		class="grid-container-block"
+		title="grid-container with display: grid (left as block container)"
+	>
+		<div class="item-1">Block display grid container</div>
 
-		<div class="flex-item item-2">css {display: grid;}</div>
-		<div class="flex-item item-3">
+		<div class="item-2">css {display: grid;}</div>
+		<div class="item-3">
 			The display prop specifies the display behavior (the type of rendering box of
 			an element)
 		</div>
-		<div class="flex-item item-4">
+		<div class="item-4">
 			Displays an element as a block element (like &lt;p> ). It starts on a new
 			line, and takes up the whole width.
 		</div>
-		<div class="flex-item item-5">
+		<div class="item-5">
 			The items have been split into two columns of auto width {
 			grid-template-columns: auto auto;}
 		</div>
-		<div class="flex-item item-6">6</div>
-		<div class="flex-item item-7">7</div>
-		<div class="flex-item item-8">8</div>
-		<div class="flex-item item-9">9</div>
-		<div class="flex-item item-10">10</div>
+		<div class="item-6">6</div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
 	</div>
 	<div class="grid-container-inline">
-		<div class="flex-item item-1">
-			A grid container with display set to Inline-grid.
+		<div class="item-1">A grid container with display set to Inline-grid.</div>
+		<div class="item-2">2</div>
+		<div class="item-3">3</div>
+		<div class="item-4">4</div>
+		<div class="item-5">5</div>
+		<div class="item-6">6</div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
+	</div>
+	<div class="grid-container-1">
+		<div class="item-1">
+			A grid container with display set to grid. Each element constrained to 1fr of
+			10 in cols.
 		</div>
-		<div class="flex-item item-2">2</div>
-		<div class="flex-item item-3">3</div>
-		<div class="flex-item item-4">4</div>
-		<div class="flex-item item-5">5</div>
-		<div class="flex-item item-6">6</div>
-		<div class="flex-item item-7">7</div>
-		<div class="flex-item item-8">8</div>
-		<div class="flex-item item-9">9</div>
-		<div class="flex-item item-10">10</div>
+		<div class="item-2">grid-template-columns: repeat(10, 1fr);</div>
+		<div class="item-3">3. Will extend in height to maintain this.</div>
+		<div class="item-4">4</div>
+		<div class="item-5">5</div>
+		<div class="item-6">6</div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
+	</div>
+	<div
+		class="grid-container-2"
+		title="10 rows only set to 1fr each(one fraction of available height of container)"
+	>
+		<div class="item-1">
+			A grid container with display set to grid. Each element constrained to 1fr of
+			10 in rows.
+		</div>
+		<div class="item-2">grid-template-rows: repeat(10, 1fr);</div>
+		<div class="item-3">3</div>
+		<div class="item-4">4</div>
+		<div class="item-5">5</div>
+		<div class="item-6">6</div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
+	</div>
+	<div
+		class="grid-container-3"
+		title="10 rows only set to 1fr each(one fraction of available height of container)"
+	>
+		<div class="item-1">A grid container with display set to grid.</div>
+		<div class="item-2"><b> grid-template-columns: repeat(10, auto);</b></div>
+		<div class="item-3">
+			With each col width set to auto, it fits the max width of the child first,
+			rather than above where the content is constrained to the grid container
+			column width declaration.
+		</div>
+		<div class="item-4">4</div>
+		<div class="item-5">5</div>
+		<div class="item-6">6</div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
+	</div>
+	<div
+		class="grid-container-4"
+		title="container 4 : 10 rows only set to 1fr each(one fraction of available height of container) "
+	>
+		<div class="item-1 col-m-12">A grid container with display set to grid.</div>
+		<div class="item-2">
+			<b> grid-template-columns: repeat(10, 1fr) after 1200px.</b>
+		</div>
+		<div class="item-3">
+			grid-template-rows: repeat(10, auto); less than 1200px Note I have rid the
+			columns! So these now stack and fit the content
+		</div>
+		<div class="item-4">
+			That is, on small devices, adjust column width to max-content(I think)
+		</div>
+		<div class="item-5">
+			What's causing the horizontal scroll is are the grids set to
+			grid-template-columns: repeat(10,1fr)
+		</div>
+		<div class="item-6">6</div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
+	</div>
+	<div
+		class="grid-container-5"
+		title="Same as above but with default setting to repeat(10,1fr)"
+	>
+		<div class="item-1">A grid container with display set to grid.</div>
+		<div class="item-2"></div>
+		<div class="item-3">
+			grid-template-rows: repeat(10, 1fr); less than 1200px Note I have rid the
+			columns! So these now stack and but are all of equal row height.
+		</div>
+		<div class="item-4">
+			Interesting that as you re-size the screen and the conent wraps, all grid
+			items get higher to ensure 1fr/10 is maintained.
+		</div>
+		<div class="item-5">
+			<b
+				>An interesting note. Here I set the background setting as template rows to
+				repeat 10 1fr, and then change the grid-template-columns after 1200 px, but
+				after 1200px the grid-template-rows is still in effect as I have not removed
+				it!
+			</b>
+		</div>
+		<div class="item-6">Hence the massive white space underneath....</div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
+	</div>
+	<div
+		class="grid-container-6"
+		title="Same as above but with default setting to repeat(10,1fr)"
+	>
+		<div class="item-1">A grid container with display set to grid.</div>
+		<div class="item-2">
+			<b>grid-template-columns: repeat(10, 1fr) after 1200px.;</b>
+		</div>
+		<div class="item-3">
+			grid-template-rows: repeat(10, 1fr); less than 1200px Note I have rid the
+			columns! So these now stack and but are all of equal row height.
+		</div>
+		<div class="item-4">
+			Interesting that as you re-size the screen and the conent wraps, all grid
+			items get higher to ensure 1fr/10 is maintained.
+		</div>
+		<div class="item-5">(5) grid-column: 3 / span 2;</div>
+		<div class="item-6">6</div>
+
+		<div class="item-7">(7) grid-column: 1 / span 4;</div>
+		<div class="item-8">8</div>
+		<div class="item-9">(9) grid-column: 7 / span 3;</div>
+		<div class="item-10">
+			(10) When adjusted for specific item col/row spans, will break order and then
+			continue. Will mess up at lower break points to maintain container defined
+			order and item placement.
+		</div>
+	</div>
+	<div
+		class="grid-container-7"
+		title="Grid container, display grid, +1200 break to 10 cols auto and justify-content center"
+	>
+		<div class="item-1">A grid container with display set to grid.</div>
+		<div class="item-2">After 1200 gtc: r(10,auto)</div>
+		<div class="item-3">Default; justify-content start</div>
+		<div class="item-4">After 1200, justify-content center</div>
+		<div class="item-5"></div>
+		<div class="item-6"></div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
+	</div>
+	<div
+		class="grid-container-8"
+		title="Grid with 5 cols 1fr 3 rows 1fr , grid-auto-flow: column "
+	>
+		<div class="item-1">1</div>
+		<div class="item-2">2</div>
+		<div class="item-3">3</div>
+		<div class="item-4">4</div>
+		<div class="item-5">5</div>
+		<div class="item-6">6</div>
+		<div class="item-7">7</div>
+		<div class="item-8">8</div>
+		<div class="item-9">9</div>
+		<div class="item-10">10</div>
 	</div>
 </template>
 
